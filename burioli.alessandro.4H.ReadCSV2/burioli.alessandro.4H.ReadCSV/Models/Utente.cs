6ae3fe2b268c58073c7083e47ebbaaefbadc38c5
Alessandro Burioli <116791054+Burioli@users.noreply.github.com>
2023-03-16 10:07:49 +0100

    Add files via upload

---
+++ burioli.alessandro.4H.ReadCSV2/burioli.alessandro.4H.ReadCSV/Models/Utente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pompa.paolofrancesco._4H.ReadCSV.Models
+{
+    public class Utente
+    {
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Tipo { get; set; }
+
+        public Utente(string str)
+        {
+            string[] colonne = str.Split(";");
+            Nome = colonne[0];
+            Cognome = colonne[1]; 
+            Email = colonne[2];
+            Tipo = colonne[3];
+            
+        }
+    }
+   
+
+}
